181704566850b72039d5b221104db0a5b1d6a5ef
Achilleas <achilleas@tessa>
2013-06-19 19:50:23 +0300

    Display raw audio data on screen and save to file after 5 secs
    Making sure the microphone code works

--- src/main/java/org/briansimulator/briandroid/Simulations/LickliderPitch.java
+++ src/main/java/org/briansimulator/briandroid/Simulations/LickliderPitch.java
@@ -1,7 +1,16 @@
 package org.briansimulator.briandroid.Simulations;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Environment;
 import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by achilleas on 19/06/13.
@@ -19,28 +28,48 @@ public class LickliderPitch extends Simulation {
     }
 
     public void run() {
-        MediaRecorder micRec = new MediaRecorder();
-        micRec.setAudioSource(MediaRecorder.AudioSource.MIC);
-        micRec.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
-        micRec.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-        micRec.setOutputFile("/dev/null");
-        try {
-            micRec.prepare();
-            micRec.start();
-        } catch (Exception e) {
-            Log.d(LOGID, "Failed to initialise microphone");
-            Log.d(LOGID, "EXCEPTION THROWN: "+e.toString());
-        }
-        double amp;
-        while (true) {
-            amp = micRec.getMaxAmplitude();
-            publishProgress("AMPLITUDE: "+amp);
+        setState(1);
+        int minBufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+
+        AudioRecord micRec = new AudioRecord(MediaRecorder.AudioSource.MIC, // microphone
+                44100, // sampling rate (default 44100 for max compatibility)
+                AudioFormat.CHANNEL_IN_MONO, // max compatibility
+                AudioFormat.ENCODING_PCM_16BIT, // 8 or 16 bit
+                4096); //1048576);                       // buffer size in bytes
+        micRec.startRecording();
+        byte[] audioBytes = new byte[4096];
+        int bytesRead;
+        ArrayList<Integer> audioData = new ArrayList<Integer>();
+        long start = System.currentTimeMillis();
+        while (System.currentTimeMillis()-start < 5000) {
+            bytesRead = micRec.read(audioBytes, 0, 4096);
+            for (byte b : audioBytes) {
+                audioData.add((int)b);
+            }
+            publishProgress("RAW DATA: " + Arrays.toString(audioBytes) +
+                    "\nBytes: " + bytesRead +
+                    "\nMin buffer size: " + minBufferSize);
             try {
-                Thread.sleep(100);
+                //Thread.sleep(100);
             } catch (Exception e) {
-                Log.d(LOGID, "Exception thrown during sleep: "+e.toString());
+                Log.e(LOGID, "Exception thrown during sleep. ");
+                e.printStackTrace();
             }
         }
-
+        try{
+            File sdCard = Environment.getExternalStorageDirectory();
+            File dir = new File(sdCard.getAbsolutePath()+"/briandroid.tmp/"); //TODO: optional save path
+            dir.mkdirs();
+            String audioFilename = "audio.raw";
+            File audioFile = new File(dir, audioFilename);
+            FileOutputStream streamWriter = new FileOutputStream(audioFile);
+            streamWriter.write(audioData.toString().getBytes()); // I'm aware of how ridiculous this is
+            streamWriter.close();
+        } catch (Exception e) {
+            Log.e(LOGID, "Exception thrown while writing file");
+            e.printStackTrace();
+        }
+        publishProgress("ALL DONE!");
+        setState(2);
     }
 }
