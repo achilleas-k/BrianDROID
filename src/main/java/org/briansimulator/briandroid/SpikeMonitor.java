afe5ba7f204ea2871bf9704ecec2639715052753
Achilleas <achilleas@morgana>
2013-09-19 15:31:51 +0300

    Save code in SpikeMonitor

--- src/main/java/org/briansimulator/briandroid/SpikeMonitor.java
+++ src/main/java/org/briansimulator/briandroid/SpikeMonitor.java
@@ -1,10 +1,16 @@
 package org.briansimulator.briandroid;
 
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 /**
  * Created by achilleas on 16/09/13.
  */
+
 class Spike {
 
     public final int i;
@@ -18,6 +24,7 @@ class Spike {
 
 public class SpikeMonitor {
 
+    private final static String LOGID = "org.briansimulator.briandroid.Spikemonitor";
     public ArrayList<Spike> spikes;
     public int nspikes = 0;
     public int max_idx = 0;
@@ -86,4 +93,50 @@ public class SpikeMonitor {
         }
         return spikeArray;
     }
+
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * Save the spikes recorded by this SpikeMonitor to a text file on the
+     * device's external storage.
+     * The data is formatted in two rows of equal length, the first representing
+     * the spike times while the second represents the neuron indices.
+     *
+     * The default save directory is ``/sdcard/BrianDROIDout/``.
+     * Optional paths will be available in a future release.
+     *
+     */
+    public void writeToFile(String filename) {
+        if (isExternalStorageWritable()) {
+            StringBuilder nrnSB = new StringBuilder();
+            StringBuilder timeSB = new StringBuilder();
+            for (Spike spk : spikes) {
+                nrnSB.append(spk.i+" ");
+                timeSB.append(spk.t+" ");
+            }
+            nrnSB.append("\n");
+            timeSB.append("\n");
+            try {
+                File sdCard = Environment.getExternalStorageDirectory();
+                //TODO: optional save path
+                File dir = new File(sdCard.getAbsolutePath()+"/BrianDROIDout/");
+                dir.mkdirs();
+                File spikesFile = new File(dir, filename);
+                FileOutputStream spikesStream = new FileOutputStream(spikesFile);
+                spikesStream.write(nrnSB.toString().getBytes()); // this seems weird
+                spikesStream.write(timeSB.toString().getBytes());
+                spikesStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
